df508882daf3c6c8de0d48b0ae686cda4ea7c615
nktwoo <brnbkent2804@gmail.com>
2021-07-27 14:47:09 +0300

    'UPD'

--- handlers/users/purchase.py
+++ handlers/users/purchase.py
@@ -1,11 +1,11 @@
 from aiogram.dispatcher.filters import Command
 from aiogram.types import Message
 
-from keyboards.inline.choise_buttons import choice
+from keyboards.inline.choice_buttons import choice
 from loader import dp
 
 
-@dp.message_handlers(Command('items'))
+@dp.message_handler(Command('items'))
 async def show_items(message: Message):
     await message.answer(text='На продажу у нас есть 2 товара: 5 Яблок и 1 Груша. \n'
                               'Есил ничего не нужно - жмите отмену',
